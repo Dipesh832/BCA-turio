9af982741277102f581f1dc54b1ab8c3cad67020
Dipesh Tharu <dipeshtharu444@gmail.com>
2024-12-25 21:39:57 +0545

    #add initial file

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        autoplay:true,
+        nav: true,
+        navText:['<i class="fa-solid fa-arrow-up"></i>','<i class="fa-solid fa-arrow-down"></i>']
+    });
+  });
